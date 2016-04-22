c90dadc33bbcaa083fc9a1f098a0b9d1dbbd40f9
Walid Arghandewal <walid.argh@gmail.com>
2016-04-22 11:55:49 -0700

    Database Seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+lat_min = 37
+lng_min = -123
+offset = 0.001
+def rand_lat
+	lat_min = 37
+	offset = 0.001
+	lat_min + offset*rand(1000)
+end
+
+def rand_lng
+	lng_min = -123
+	offset = 0.001
+	lng_min + offset*rand(1000)
+end
+
+def description
+	Faker::Hipster.sentence
+end
+
+
+100.times do
+	Bench.create!(description: description, lat: rand_lat, lng: rand_lng)
+end
